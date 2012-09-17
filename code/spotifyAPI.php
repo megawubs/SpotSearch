187364512380b8f1f868fc7e5cbc093fe4a26444
megawubs <bwubs@me.com>
2012-09-17 21:18:51 +0200

    started on parsing the json result
    Made code/search.php way less messy. Added a buch of private and a few
    public functions to code/spotifyAPI.
    Disabled space escaping in info.plist

--- code/spotifyAPI.php
+++ code/spotifyAPI.php
@@ -10,8 +10,11 @@ class spotifyAPI {
       *  tr => track
       *  al => album
       *  ar => artist
+      *
+      *  added spaces to the key's to make sure it is a search string with
+      *  specified search option
       */
-    public $choices = array('tr','al', 'ar');
+    public $choices = array('tr '=>'track','al '=>'album', 'ar '=>'artist');
     /**
       *  the spotify api url 
       */
@@ -21,7 +24,7 @@ class spotifyAPI {
       */
     private $baseLink = 'http://ws.spotify.com/search/1/';
     /**
-      *  the  
+      *  the varible that contains the format we want out data to recieve in
       */
     private $format = '.json';
     /**
@@ -32,35 +35,34 @@ class spotifyAPI {
       *  contains the choice 
       */
     private $choice;
+    /**
+      *  the json object 
+      */    
+    private $json;
+    /**
+      *  array of the search result 
+      */
+    public $result;
     /*
      * __construct()
      * @param $query
      */
     
     public function __construct($query) {
-        foreach($this->choices as $kind){
-            if(substr($query, 0, 2) == $kind){
-                $choice = $kind;
-                $this->query = substr($query, 2);
-            }
-            else{
-                $choice = 'tr';
-                $this->query = $query;
+        //echo $query;
+        $choice = '';
+        foreach($this->choices as $key=>$kind){
+            if(substr($query, 0, 3) == $key){
+                $choice = $this->choices[$key];
+                $this->query = substr($query, 3);
             }
         }
-        switch($choice){
-            case "tr":
-                $kind = 'track';
-                break;
-            case "al":
-                $kind = 'album';
-                break;
-            case 'ar':
-                $kind = 'artist';
-                break;
+        if(empty($choice)){
+            $choice = $this->choices['tr '];
         }
-        $this->choice = $kind;
+        $this->choice = $choice;
         $this->make_url();
+        //echo $this->choice;
     }
     
     /*
@@ -74,9 +76,84 @@ class spotifyAPI {
     
     private function make_url() {
         $searchPattern = "?q=".htmlentities($this->query);
-        $searchPattern = str_replace('\ ', '%20', $searchPattern);
-        $this->url = $this->baseLink.$this->kind.$this->format.$searchPattern;
+        $searchPattern = str_replace(' ', '%20', $searchPattern);
+        $this->url = $this->baseLink.$this->choice.$this->format.$searchPattern;
     }
+    
+    /*
+     * fetches $this->url and makes an object of the json return
+     * stores it in $this->json
+     *
+     * function fetchUrl
+     * @return this
+     */
+    
+    public function fetchUrl() {
+        $json = file_get_contents($this->url);
+        $this->json = json_decode($json);
+        return $this;
+    }
+    /**
+      *  parses the json object
+      *
+      *  @return this
+      */
+    public function parseJson(){
+        print_r($this->json);
+        if($this->choice == 'track'){
+            //we're dealing with a lot of data!
+            $result = $this->parseTracks();
+        }
+        elseif($this->choice == 'album'){
+            //also a lot of data!
+           $result = $this->parseAlbums();
+        }
+        elseif($this->choice == 'artist'){
+            //easy data!
+            $result = $this->parseArtist();
+        }
+        $this->result = $result;
+    }
+    
+    /*
+     * parses the result from a track search
+     *
+     * function parseTracks
+     * @param 
+     * @throws 
+     * @return array
+     */
+    
+    private function parseTracks() {
+        //parse all tracks and return an array with name and spotify uri
+    }
+    
+    /*
+     * parses the result fomr a album search
+     *
+     * function parseAlbum
+     * @param 
+     * @throws 
+     * @return Array
+     */
+    
+    private function parseAlbum() {
+        //parse all albums an return an array with name and spotify uri
+    }
+    
+    /*
+     * pases an artist search
+     *
+     * function parseArtist
+     * @param 
+     * @throws 
+     * @return Array
+     */
+    
+    private function parseArtist() {
+        //parse the artist result and return an array with name and spotify uri
+    }
+    
 }
 
 ?>
